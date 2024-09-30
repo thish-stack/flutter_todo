91cef02c66eae7bb3fe0bce7693506e1d64baeb2
thish-stack <leodo5159@gmail.com>
2024-09-30 10:49:17 +0530

    second

--- lib/utils/todo_list.dart
+++ lib/utils/todo_list.dart
@@ -11,7 +11,7 @@ class TodoList extends StatelessWidget {
     required this.onChanged,
     required this.deleteFunction,
     required this.editFunction,
-    required this.taskDate, 
+    required this.taskDate,
   });
 
   final String taskName;
@@ -21,193 +21,205 @@ class TodoList extends StatelessWidget {
   final Function(BuildContext)? editFunction;
   final String taskDate;
 
-  void _showTaskDetailsBottomSheet(BuildContext context, String taskName, String taskDate) {
-  showModalBottomSheet(
-    context: context,
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-    ),
-    builder: (BuildContext context) {
-      return Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Task Name:',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).primaryColor,
-              ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              taskName,
-              style: const TextStyle(
-                fontSize: 16,
-                color: Colors.black,
-              ),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Task Date:',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).primaryColor,
-              ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              taskDate,
-              style: const TextStyle(
-                fontSize: 16,
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-      );
-    },
-  );
-}
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: GestureDetector(
-        onTap: () {
-          _showTaskDetailsBottomSheet(context, taskName, taskDate);
-        },
-      child: Slidable(
-        endActionPane: ActionPane(
-          motion: const StretchMotion(),
-          children: [
-            // SlidableAction(
-            //   onPressed: editFunction,
-            //   icon: Icons.edit,
-            //   backgroundColor: Colors.blue,
-            //   borderRadius: BorderRadius.circular(15),
-            // ),
-            SlidableAction(
-              onPressed: (context) {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: const Text('Delete Task'),
-                      content: const Text(
-                          'Are you sure you want to delete this task?'),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop(); 
-                          },
-                          child: const Text('No'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            deleteFunction!(
-                                context); 
-                            Navigator.of(context).pop(); 
-                          },
-                          child: const Text('Delete'),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-              icon: Icons.delete,
-              foregroundColor: Colors.red,
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ],
-        ),
-        child: Container(
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            gradient: const LinearGradient(
-              colors: [Color(0xFFE2267F), Color(0xFFE94E97), Color(0xFFEF6DAB)],
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-            ),
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.pink.withOpacity(1),
-                blurRadius: 10,
-                offset: const Offset(0, 5),
-              ),
-            ],
-          ),
-          child: Row(
-            mainAxisAlignment:
-                MainAxisAlignment.spaceBetween, 
+  void _showTaskDetailsBottomSheet(
+      BuildContext context, String taskName, String taskDate) {
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (BuildContext context) {
+        return Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-             
-              Row(
-                children: [
-                  Checkbox(
-                    value: taskCompleted,
-                    onChanged: onChanged,
-                    checkColor: Colors.black,
-                    activeColor: Colors.white,
-                    side: const BorderSide(
-                      color: Colors.white,
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        width: 200, 
-                        child: Text(
-                          taskName,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            decoration: taskCompleted
-                                ? TextDecoration.lineThrough
-                                : TextDecoration.none,
-                            decorationColor: Colors.white,
-                            decorationThickness: 2,
-                          ),
-                          overflow: TextOverflow.fade,
-                          maxLines: 1,
-                          softWrap: false,
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        taskDate, 
-                        style: const TextStyle(
-                          color: Colors.white70,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+              Text(
+                'Task Name:',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryColor,
+                ),
               ),
-            
-              IconButton(
-                onPressed: () => editFunction!(context), 
-                icon: const Icon(
-                  Icons.edit,
-                  color: Colors.white,
+              const SizedBox(height: 8),
+              Text(
+                taskName,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                'Task Date:',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                taskDate,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
                 ),
               ),
             ],
           ),
-        ),
-      ),
-    )
+        );
+      },
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(20),
+        child: GestureDetector(
+          onTap: () {
+            _showTaskDetailsBottomSheet(context, taskName, taskDate);
+          },
+          child: Slidable(
+            endActionPane: ActionPane(
+              motion: const StretchMotion(),
+              children: [
+                // SlidableAction(
+                //   onPressed: editFunction,
+                //   icon: Icons.edit,
+                //   backgroundColor: Colors.blue,
+                //   borderRadius: BorderRadius.circular(15),
+                // ),
+                SlidableAction(
+                  onPressed: (context) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Delete Task'),
+                          content: const Text(
+                              'Are you sure you want to delete this task?'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('No'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                deleteFunction!(context);
+                                Navigator.of(context).pop();
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text('Task deleted'),
+                                    // action: SnackBarAction(
+                                    //   label: 'Undo',
+                                    //   onPressed: () {
+
+                                    //     //undoDeleteTask(context);
+                                    //   },
+                                    // ),
+                                    duration: Duration(seconds: 3),
+                                  ),
+                                );
+                              },
+                              child: const Text('Delete'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  icon: Icons.delete,
+                  foregroundColor: Colors.red,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ],
+            ),
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [
+                    Color(0xFFE2267F),
+                    Color(0xFFE94E97),
+                    Color(0xFFEF6DAB)
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.pink.withOpacity(1),
+                    blurRadius: 10,
+                    offset: const Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: taskCompleted,
+                        onChanged: onChanged,
+                        checkColor: Colors.black,
+                        activeColor: Colors.white,
+                        side: const BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 200,
+                            child: Text(
+                              taskName,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                decoration: taskCompleted
+                                    ? TextDecoration.lineThrough
+                                    : TextDecoration.none,
+                                decorationColor: Colors.white,
+                                decorationThickness: 2,
+                              ),
+                              overflow: TextOverflow.fade,
+                              maxLines: 1,
+                              softWrap: false,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            taskDate,
+                            style: const TextStyle(
+                              color: Colors.white70,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    onPressed: () => editFunction!(context),
+                    icon: const Icon(
+                      Icons.edit,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
