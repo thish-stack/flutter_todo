d6499cf1b3867a0daa8116f487f534c5791a0868
thish-stack <leodo5159@gmail.com>
2024-10-18 12:40:48 +0530

    Added queries in sep. file

---
+++ lib/database/database_queries.dart
@@ -0,0 +1,21 @@
+// database_queries.dart
+
+import 'database_constants.dart';
+
+class DatabaseQueries {
+  // Initialization script to create the tasks table
+  static final List<String> initialScript = [
+    '''CREATE TABLE ${DatabaseConstants.tableName} (
+      ${DatabaseConstants.columnId} INTEGER PRIMARY KEY AUTOINCREMENT,
+      ${DatabaseConstants.columnName} TEXT,
+      ${DatabaseConstants.columnCompleted} INTEGER,
+      ${DatabaseConstants.columnDate} TEXT,
+      ${DatabaseConstants.columnDescription} TEXT,
+      '''
+  ];
+
+  // Migration scripts to alter the tasks table
+  static final List<String> migrations = [
+    '''ALTER TABLE ${DatabaseConstants.tableName} ADD COLUMN ${DatabaseConstants.columnDescription} TEXT'''
+  ];
+}
